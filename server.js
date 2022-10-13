f52877e11b6e23df7f847c30ae24aa695b084c46
Joe P Shoulak <joe@squarecompass.io>
2022-10-13 14:40:27 -0500

    all routes added

--- server.js
+++ server.js
@@ -5,16 +5,54 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.port || 3001;
 
+/* == MIDDLEWARE == */
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/notes', (req, res) => 
-    res.json(JSON.parse(fs.readFileSync("./db/db.json")))
-);
+/* == API ROUTES == */
+app.get('/api/notes', (req, res) => {
+    console.log("(API) GET Notes called.");
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/index.html'))
-);
+    return res.json(JSON.parse(fs.readFileSync("./db/db.json")));
+});
 
+app.post('/api/notes', (req, res) => {
+    console.log("(API) POST Notes called.");
+
+    let oldData = JSON.parse(fs.readFileSync("./db/db.json"));
+    let maxID = oldData.map(d => parseInt(d.id)).pop();
+    
+    let newNote = req.body;
+    newNote["id"] = maxID + 1;
+
+    return fs.writeFileSync("./db/db.json", JSON.stringify([...oldData, newNote]));
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    console.log("(API) DELETE Notes called.");
+
+    let oldData = JSON.parse(fs.readFileSync("./db/db.json"));
+    let newData = oldData.filter(d => d.id != req.params.id)
+
+    return fs.writeFileSync("./db/db.json", JSON.stringify(newData));
+});
+
+/* == HTML ROUTES == */
+
+app.get('/notes', (req, res) => {
+    console.log("(HTML) GET Notes called.");
+
+    return res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    console.log("(HTML) GET * route. Rendering index.html");
+
+    return res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+/* == LISTEN == */
 app.listen(PORT, () =>
   console.log(`Serving static asset routes at http://localhost:${PORT}`)
 );
