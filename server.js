d5e1ee2051c873daf4248ef3579c3d12fa4967a1
Joe P Shoulak <joe@squarecompass.io>
2022-10-13 12:36:27 -0500

    working on implementing DOM rendering

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.port || 3001;
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/api/notes', (req, res) => 
+    res.json(JSON.parse(fs.readFileSync("./db/db.json")))
+);
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes at http://localhost:${PORT}`)
