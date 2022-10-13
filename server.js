d77512525367ca964a726c7c9dfceb17232eb360
Joe P Shoulak <joe@squarecompass.io>
2022-10-13 11:56:42 -0500

    * route works now

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.port || 3001;
 
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => res.sendFile('notes.html'));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes at http://localhost:${PORT}`)
